2e75456a8943bfdba3802fade5e08b1a634deafa
ennaed11 <deanne@Deannes-MacBook-Pro.local>
2018-10-01 01:06:28 +0800

    Added bottom navigation in HomeScreen

--- Components/Screens/HomeScreen.js
+++ Components/Screens/HomeScreen.js
@@ -1,23 +1,107 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View, StatusBar } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons.js';
 
 import Screen from './Screen';
 
-export default class HomeScreen extends Component<{}> {
-  static navigationOptions = {
-    headerMode: 'none',
-    header: null,
-  };
+class Home extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar barStyle = 'dark-content' hidden = {false}/>
-        <Text style={styles.welcome}>Homescreennnnn.</Text>
+      <View style= {styles.container}>
+        <Text style= {styles.welcome}> HOME! </Text>
       </View>
     );
   }
 }
+
+class StreakScreen extends Component {
+  render() {
+    return (
+      <View style= {styles.container}>
+        <Text style= {styles.welcome}> STREAK! </Text>
+      </View>
+    );
+  }
+}
+
+class ReadingScreen extends Component {
+  render() {
+    return (
+      <View style= {styles.container}>
+        <Text style= {styles.welcome}> HOME! </Text>
+      </View>
+    );
+  }
+}
+
+class GraphScreen extends Component {
+  render() {
+    return (
+      <View style= {styles.container}>
+        <Text style= {styles.welcome}> HOME! </Text>
+      </View>
+    );
+  }
+}
+
+class ShareDataScreen extends Component {
+  render() {
+    return (
+      <View style= {styles.container}>
+        <Text style= {styles.welcome}> HOME! </Text>
+      </View>
+    );
+  }
+}
+
+export default createMaterialBottomTabNavigator ({
+  Home: { screen: Home,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon:({tintColor}) => (
+        <Icon name= 'ios-home' color= {tintColor} size= {24} />
+      )
+    }
+  },
+  Streak: { screen: StreakScreen,
+    navigationOptions: {
+      tabBarLabel: 'Streaks',
+      tabBarIcon:({tintColor}) => (
+        <Icon name= 'ios-star' color= {tintColor} size= {24} />
+      )
+    }
+  },
+  Reading: { screen: ReadingScreen,
+    navigationOptions: {
+      tabBarLabel: 'Add Reading',
+      tabBarIcon:({tintColor}) => (
+        <Icon name= 'ios-water' color= {tintColor} size= {24} />
+      )
+    }
+  },
+  Graph: { screen: GraphScreen,
+    navigationOptions: {
+      tabBarLabel: 'Stats',
+      tabBarIcon:({tintColor}) => (
+        <Icon name= 'ios-stats' color= {tintColor} size= {24} />
+      )
+    }
+  },
+  SendData: { screen: ShareDataScreen,
+    navigationOptions: {
+      tabBarLabel: 'Send Data',
+      tabBarIcon:({tintColor}) => (
+        <Icon name= 'ios-document' color= {tintColor} size= {24} />
+      )
+    }
+  }
+}, {
+  activeTintColor: 'blue',
+  barStyle: {
+    backgroundColor: 'white',
+  },
+})
 
 const styles = StyleSheet.create({
   container: {
@@ -31,11 +115,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
 });
 
-AppRegistry.registerComponent('HomeScreen', () => HomeScreen);
+//AppRegistry.registerComponent('HomeScreen', () => HomeScreen);
